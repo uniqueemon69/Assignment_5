0448d86b2fc7aa46eb1369a3a457c4c35a084f12
uniqueemon69 <mahmudulhasan@iut-dhaka.edu.com>
2020-03-06 16:27:31 +0600

    final

--- special.php
+++ special.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html>
 <head>
     <title>
@@ -18,11 +18,13 @@
                 $img_name = uniqid().".jpg"; //gives each uploaded file a unique name so that they may not collide
                 move_uploaded_file($tmp, "photos/".$img_name); //upload(the temporary directory that was create when file uploaded, the destiantion)
             }
-            else if($_FILES['image']['size'] ==0) else echo "Pls choose a file";
+            //else if($_FILES['image']['size'] ==0) echo "Pls choose a file";
             else echo "File Not supported";
         }
     ?>
     <form method="POST" action="" enctype="multipart/form-data">
+    	<input type="text" name="name" placeholder="Enter your name"><br>
+    	<input type="email" name="email" placeholder="Enter your email"><br>
         <input type="file" name="image" accept="image/*"><br>
         
         <!-- type can be the type of input such as button(type is submit), dropdown etc -->
