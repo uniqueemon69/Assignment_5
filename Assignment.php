537cba4a6d76e84c84edca831f1f73eab3af8cad
uniqueemon69 <mahmudulhasan@iut-dhaka.edu.com>
2020-03-06 13:19:22 +0600

    1st

---
+++ Assignment.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Forms
+    </title>
+</head>
+<body>
+    <?php
+   
+        if(isset($_POST['upload']))
+        {
+            
+            if(( $_FILES['image']['type'] == 'image/jpeg' || $_FILES['image']['type'] =='image/png') && $_FILES['image']['size'] <  5000000) // to restrict type of files to be accepted
+            {
+                $tmp = $_FILES['image']['tmp_name']; //directory of the uploaded image
+                //$img_name = $_FILES['image']['name']; //name of the uploaded image
+                $img_name = uniqid().".jpg"; //gives each uploaded file a unique name so that they may not collide
+                move_uploaded_file($tmp, "photos/".$img_name); //upload(the temporary directory that was create when file uploaded, the destiantion)
+            }
+            else echo "Not supported";
+        }
+    ?>
+    <form method="POST" action="" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*"><br>
+        <!-- type can be the type of input such as button(type is submit), dropdown etc -->
+        <!--name is the name of the attribute that will be used in php-->
+        <!-- value is the name that will be displayed on the button or input -->
+        <input type="submit" name="upload" value="upload">
+    </form>
+ 
+ 
+</body>
+</html>
